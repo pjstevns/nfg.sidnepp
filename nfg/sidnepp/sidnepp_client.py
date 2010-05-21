243c6e21bbfe4864298bd269a6b0ec9c0015773e
Paul J Stevens <paul@nfg.nl>
2010-05-21 22:50:12 +0200

    wip

---
+++ nfg/sidnepp/sidnepp_client.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+# interface for SIDN EPP 
+#
+# license: GPLv3
+#
+# copyright 2010, NFG Net Facilities Group BV, www.nfg.nl
+#       
+# Paul Stevens, paul@nfg.nl
+
+from zope.interface import implements
+from interfaces import IEpp
+import socket, struct, ssl
+from lxml import etree
+
+STATE_INIT      = 0x01
+STATE_CONNECTED = 0x02
+STATE_SESSION   = 0x04
+STATE_LOGGEDIN  = 0x08
+
+DEBUG=1
+
+testserver = 'testdrs.domain-registry.nl'
+testport   = 700
+testuser   = '300271'
+testpass   = 'c2155d6292'
+
+
+def debug(msg=None):
+    if msg and DEBUG: print msg
+
+from sidnepp_protocol import SIDNEppProtocol
+
+class SIDNEppClient(SIDNEppProtocol):
+    implements(IEpp)
+
+    _connection_State = STATE_INIT
+    
+    # server frames
+    _greeting = None
+    _login = None
+
+    def __init__(self, host=None, port=None, username=None, password=None,
+                 ssl=True):
+        super(SIDNEppClient, self).__init__()
+        self.host = host
+        self.port = port
+        self.ssl = ssl
+        self.connect()
+        self._greeting = '<?xml version="1.0" encoding="UTF-8"?>%s' % \
+                etree.tostring(self.hello())
+        if username and password:
+            self._login = '<?xml version="1.0" encoding="UTF-8"?>%s' % \
+                    etree.tostring(self.login(username, password))
+
+    def connect(self):
+        assert(self._connection_State == STATE_INIT) 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        if self.ssl:
+            self._fd = ssl.wrap_socket(s)
+        else:
+            self._fd = s
+        self._connection_State = STATE_CONNECTED
+
+    def write(self, message):
+        assert(self._connection_State > STATE_INIT)
+        # validate
+        self.parse(message)
+        l = len(message)+4
+        data = struct.pack(">L", l)
+        self._fd.send(data)
+        self._fd.send(message)
+        return self.read()
+
+    def read(self):
+        buf = self._fd.recv(4)
+        need = struct.unpack(">L", buf)
+        need = need[0]-4
+        return self.parse(self._fd.recv(need))
+
+    def getState(self):
+        return self._connection_State
+
+# commands
+    def hello(self):
+        assert(self._connection_State == STATE_CONNECTED)
+        xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+          <hello/>
+        </epp>"""
+        result = self.write(xml)
+        assert(self.query(result,"//epp:greeting"))
+        self._connection_State = STATE_SESSION
+        return result
+
+    def login(self, login, password, newpassword=None, lang='NL'):
+        assert(self._connection_State == STATE_SESSION)
+        xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+        <command>
+        <login>
+        <clID>%s</clID>
+        <pw>%s</pw>
+        <options>
+        <version>1.0</version>
+        <lang>%s</lang>
+        </options>
+        <svcs>
+        <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+        <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
+        <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+        <svcExtension>
+        <extURI>urn:ietf:params:xml:ns:sidn-ext-epp-1.0</extURI>
+        </svcExtension>
+        </svcs>
+        </login>
+        </command>
+        </epp>  
+        """ % (login, password, lang)
+        result = self.write(xml)
+        r = self.query(result, "//epp:result")
+        if not r:
+            result = self.read()
+            r = self.query(result, "//epp:result")
+        self._connection_State = STATE_LOGGEDIN
+        return result
+
+    def logout(self):
+        assert(self._connection_State == STATE_LOGGEDIN) 
+        xml = """<?xml version="1.0" encoding="UTF-8"?>
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0" 
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+        <command>
+        <logout/>
+        </command>
+        </epp>
+        """
+        res = self.write(xml)
+        self._fd.close()
+        self._connection_State = STATE_INIT
+        return res
+
+    def poll(self, ack=None):
+        assert(self._connection_State == STATE_LOGGEDIN) 
+        xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?> 
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+        <command>
+        <poll op="%s" %s/>
+        </command>
+        </epp>
+        """
+        op='req'
+        msgid=''
+        if ack:
+            op='ack'
+            msgid='msgID="%s"' % ack
+        return self.write(xml % (op, msgid))
+
+    def domain_check(self, domain):
+        xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+        <command>
+        <check>
+        <domain:check xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+        <domain:name>%s</domain:name>
+        </domain:check>
+        </check>
+        </command>
+        </epp>
+        """ % domain
+        return self.write(xml)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
--- nfg/sidnepp/tests.py
+++ nfg/sidnepp/tests.py
@@ -6,10 +6,35 @@ from lxml import etree
 
 import unittest
 
-testuser='300271'
-testpass='c2155d6292'
+testserver = 'testdrs.domain-registry.nl'
+testport   = 700
+testuser   = '300271'
+testpass   = 'c2155d6292'
 
-class testSidnEPP(unittest.TestCase):
+class testSidnEppProtocol(unittest.TestCase):
+
+    xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    <command>
+    <login>
+    <clID>username</clID>
+    <pw>password</pw>
+    <options>
+    <version>1.0</version>
+    <lang>nl</lang>
+    </options>
+    <svcs>
+    <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+    <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
+    <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+    <svcExtension>
+    <extURI>urn:ietf:params:xml:ns:sidn-ext-epp-1.0</extURI>
+    </svcExtension>
+    </svcs>
+    </login>
+    </command>
+    </epp>"""
+
 
     def setUp(self):
         self.o = SIDNEppProtocol()
@@ -20,30 +45,12 @@ class testSidnEPP(unittest.TestCase):
 #        except AssertionError: 
 #            pass
 
-    def testQuery(self):
-        xml = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-        <command>
-        <login>
-        <clID>username</clID>
-        <pw>password</pw>
-        <options>
-        <version>1.0</version>
-        <lang>nl</lang>
-        </options>
-        <svcs>
-        <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
-        <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
-        <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
-        <svcExtension>
-        <extURI>urn:ietf:params:xml:ns:sidn-ext-epp-1.0</extURI>
-        </svcExtension>
-        </svcs>
-        </login>
-        </command>
-        </epp>"""
+    def test_parse(self):
+        e = self.o.parse(self.xml)
+        print e
 
-        e = etree.fromstring(xml, self.o.parser)
+    def testQuery(self):
+        e = self.o.parse(self.xml)
         r1 = e.xpath('//epp:command', namespaces={'epp':"urn:ietf:params:xml:ns:epp-1.0"})
         self.failUnless(len(r1) == 1)
         r2 = self.o.query(e, "//epp:command")
@@ -51,11 +58,18 @@ class testSidnEPP(unittest.TestCase):
         r3 = self.o.query(e,"//epp:login")
         self.failUnless(type(r3) == type(r2))
 
-#    def testLogin(self):
-#        s = self.o.login(testuser, testpass)
-#        r = self.o.query(s, '//epp:access')
-#        self.failUnless(len(r) > 0)
-#
+from sidnepp import SIDNEppClient
+
+class testSIDNEppClient(unittest.TestCase):
+
+    def setUp(self):
+        self.o = SIDNEppClient(testserver,testport)
+
+    def testLogin(self):
+        s = self.o.login(testuser, testpass)
+        r = self.o.query(s, '//epp:result')
+        self.failUnless(len(r) > 0)
+
 #    def testLogout(self):
 #        s = self.o.login(testuser, testpass)
 #        r = self.o.query(s, '//epp:access')[0]
@@ -63,10 +77,11 @@ class testSidnEPP(unittest.TestCase):
 #        r = self.o.query(s, '//epp:result')[0]
 #        self.failUnless(r.get("code") == "1000")
 #
-#    def testPoll(self):
-#        self.o.login(testuser, testpass)
-#        s = self.o.poll()
-        #print etree.tostring(s)
+    def testPoll(self):
+        s = self.o.login(testuser, testpass)
+        print etree.tostring(s)
+        s = self.o.poll()
+        print etree.tostring(s)
 
 #    def testDomainCheck(self):
 #        self.o = SIDNEppClient('testdrs.domain-registry.nl',700,testuser,testpass)
